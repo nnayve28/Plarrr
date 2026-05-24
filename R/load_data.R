0ea5e7e26307caf8ea51fe0a025f1026a4f66e26
Noah Nayve <npnayve@gmail.com>
2026-05-23 22:16:27 -0700

    Initial commit

---
+++ R/load_data.R
@@ -0,0 +1,18 @@
+#' Load plastic waste data
+#'
+#' Loads the plastic waste dataset directly
+#' from TidyTuesday
+#'
+#' @return A data frame with columns including country, year, parent_company,
+#'   plastic type totals (hdpe, ldpe, pet, pp, ps, pvc), grand_total,
+#'   num_events, and volunteers.
+#' @export
+#' @importFrom readr read_csv
+#' @examples
+#' df <- load_data()
+#' head(df)
+load_data <- function() {
+  readr::read_csv(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv'
+  )
+}
